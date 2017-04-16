acf6bb56306a7f2812068cf92dbbd2ac49fdc22c
as3mbus <asembus@gmail.com>
2017-04-16 12:23:20 +0700

    still testing

--- web/index.php
+++ web/index.php
@@ -26,5 +26,9 @@ $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
+$app->get('/announcement', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
 
 $app->run();
